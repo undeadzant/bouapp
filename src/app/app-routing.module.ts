787626c00beb9f13d35d3df63eb7ce6c5dc62b9e
Zant <mjallemand@outlook.com>
2019-05-04 01:42:02 -0700

    Added About and Home page, Setup app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BoucherieComponent } from './boucherie/boucherie.component';
 import { VendorComponent } from './vendor.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
+// here you can tell Angular how to route these paths
+// For example ./about/about can be routed to just about
+// so when you are on the site /about will go to the about page
 const routes: Routes = [
- // { path: '', component: BoucherieComponent},
- // { path: 'boucherie', component: BoucherieComponent},
-  { path: 'vendor', component: VendorComponent}
+  { path: '', component: HomeComponent},
+ // { path: 'about/about', component: AboutComponent},
+  { path: 'vendor', component: VendorComponent},
+  { path: 'about', component: AboutComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,25 @@ import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 import {MatInputModule} from '@angular/material/input';
 import {MatCardModule} from '@angular/material/card';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule, MatMenu} from '@angular/material/menu';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VendorComponent } from './vendor.component';
 import { BoucherieComponent } from './boucherie/boucherie.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent, VendorComponent, BoucherieComponent
+    AppComponent, VendorComponent, BoucherieComponent, HomeComponent, AboutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    [MatButtonModule, MatCheckboxModule, MatInputModule, MatCardModule, MatToolbarModule]
+    [MatButtonModule, MatCheckboxModule, MatInputModule, MatCardModule, MatToolbarModule, MatIconModule, MatMenuModule]
   ],
   providers: [],
   bootstrap: [AppComponent]
